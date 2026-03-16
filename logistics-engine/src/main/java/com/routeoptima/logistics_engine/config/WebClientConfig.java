bf831027a9f991bf8576a2f14c3863e63c5df59c
Haridev-AV <haridevav2002@gmail.com>
2026-03-16 11:28:27 +0530

    Refactor: Restructure to Full-Stack monorepo with Spring Boot and Angular

---
+++ logistics-engine/src/main/java/com/routeoptima/logistics_engine/config/WebClientConfig.java
@@ -0,0 +1,16 @@
+// src/main/java/com/routeoptima/logistics_engine/config/WebClientConfig.java
+
+package com.routeoptima.logistics_engine.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class WebClientConfig {
+
+    @Bean
+    public WebClient.Builder webClientBuilder() {
+        return WebClient.builder();
+    }
+}
